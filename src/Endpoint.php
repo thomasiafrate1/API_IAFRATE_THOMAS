f651f0c7a66a0fceb2d32eda3ef7e6dc5ea1d74f
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-10-19 14:10:15 +0200

    Update Endpoint

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -4,24 +4,43 @@ class Endpoint {
 
     public static function Routing() {
         include('Tri.php');
-        if (isset($_GET['method']) && isset($_GET['tab'])) {
-            $method = $_GET['method'];
-            $tab = json_decode($_GET['tab'], true);
+        if (!isset($_GET['method']) || !isset($_GET['tab'])) {
+            echo json_encode('Parametres manquants.');
+            return;
+        }
+        $method = $_GET['method'];
+        $tab = json_decode($_GET['tab'], true);
 
+        if (json_last_error()) {
+            echo json_encode('Le tableau contient une ou plusieurs lettres.');
+            return;
+        }
 
-            switch($method){
-                case $method === "TriInsertion" :
-                    $sorted_method = Tri::TriInsertion($tab);
-                    echo json_encode($sorted_method);
-                    break;
-                case $method === "QuickSort" :
-                    $sorted_method = Tri::QuickSort($tab);
-                    echo json_encode($sorted_method);
-                    break;
-                default:
-                    echo json_encode('Aucune methode trouvé');
-                    break;
+        if (empty($tab)) {
+            echo json_encode('Le tableau est vide, veuillez entrer des chiffres.');
+            return;
+        }
+
+        foreach ($tab as $item) {
+            if (!is_numeric($item)) {
+                echo json_encode('Le tableau contient des valeurs non numeriques.');
+                return;
             }
         }
+
+        switch($method){
+            case $method === "TriInsertion" :
+                $sorted_method = Tri::TriInsertion($tab);
+                echo json_encode($sorted_method);
+                break;
+            case $method === "QuickSort" :
+                $sorted_method = Tri::QuickSort($tab);
+                echo json_encode($sorted_method);
+                break;
+            default:
+                echo json_encode('Aucune methode trouve');
+                break;
+        }
+
     }
 }
